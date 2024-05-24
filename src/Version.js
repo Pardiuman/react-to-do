3712293681b8d567699c9a8334c837d09f9f5084
pardiuman <iludhankhar58@gmail.com>
2024-05-24 12:54:58 +0530

    add a new component in react application

---
+++ src/Version.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Version = () => {
+  return (
+    <div className="version">
+      Application Revision: 1
+    </div>
+  );
+};
+
+export default Version;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TodoList from "./TodoList";
-
+import Version from './Version';
 
 var destination = document.querySelector('#container');
 
 
 ReactDOM.render(
   <div>
+        <Version/>
 	<TodoList/>
   </div>, 
   destination
